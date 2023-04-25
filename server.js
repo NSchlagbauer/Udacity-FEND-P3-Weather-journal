e94f1dd1559ec8081b09f9d919d201abd0f13784
Nicole <85258663+NSchlagbauer@users.noreply.github.com>
2023-04-25 21:26:20 +0200

    install of npm packages and basic server
    installed npm packages express, body-parser and cors
    coded basic server startup with debug callback function

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+//Callback to debug
+function listening(){
+  console.log(`server running on port ${port}`);
+}
+
+// Initialize all route with a callback function
+// TODO
+
+// Callback function to complete GET '/all'
+// TODO
+
+// Post Route
+// TODO
