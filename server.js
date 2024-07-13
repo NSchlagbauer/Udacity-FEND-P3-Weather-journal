b692994ca44a768800a361ee83ad5ea32abbd087
Nicole <85258663+NSchlagbauer@users.noreply.github.com>
2024-07-13 15:39:23 +0200

    finished logic
    finished logic and styled a bit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
@@ -10,15 +10,15 @@ const cors = require('cors');
 const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // Cors for cross origin allowance
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
+
 
 // Setup Server
 const port = 8000;
@@ -29,11 +29,21 @@ function listening() {
   console.log(`server running on port ${port}`);
 }
 
-// Initialize all route with a callback function
-// TODO
+// Initialize /all route with a callback function
+app.get('/all', getData);
 
 // Callback function to complete GET '/all'
-// TODO
+function getData (req, res) {
+  res.send(projectData);
+}
 
-// Post Route
-// TODO
+// POST Route
+app.post('/addData', postWeatherData);
+
+function postWeatherData (req, res) {
+  projectData['temperature'] = req.body.temperature;
+  projectData['date'] = req.body.date;
+  projectData['feelings'] = req.body.feelings;
+  res.send(projectData);
+}
+
--- website/app.js
+++ website/app.js
@@ -1,7 +1,73 @@
 /* Global Variables */
 // Personal API Key for OpenWeatherMap API
-const apiKey = '0668935d32c192863d56906125f43bb4&units=imperial';
+const apiKey = '0668935d32c192863d56906125f43bb4'; // &units=metric
+const baseWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather'
+const baseGeoUrl = 'http://api.openweathermap.org/geo/1.0/zip'
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = `${d.getDate()}.${d.getMonth()+1}.${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
+
+// Event listener to add function to existing HTML DOM element
+const generateButton = document.getElementById('generate');
+generateButton.addEventListener('click', generateEntry);
+
+/* Function called by event listener */
+function generateEntry(e) {
+  let zipCode = document.getElementById('zip').value;
+  let countryCode = document.getElementById('country-code').value;
+  let feelings = document.getElementById('feelings').value;
+
+  // Get Geocoordinates for the provided ZIP and country codes
+  getGeolocationData(baseGeoUrl, zipCode, countryCode, apiKey)
+    .then(function(data) {
+      // Get weather data for the found geocoordinates
+      getWeatherData(baseWeatherUrl, data.lat, data.lon, apiKey)
+        .then(function(data) { 
+          // Add data to POST request and POST to journal 
+          postJournalEntry('/addData', {temperature: data.main.temp, date: newDate, feelings: feelings});
+          // Get project data back and refresh UI
+        })
+        .then(function() {
+          refreshUI();
+        });
+    });
+}
+
+/* Function to GET Web API Data*/
+// get geolocation from ZIP-code and country code
+const getGeolocationData = async (baseUrl, zipCode, countryCode, apiKey) => {
+  const res = await fetch(`${baseUrl}?zip=${zipCode},${countryCode}&appid=${apiKey}`);
+  return await res.json();
+}
+
+const getWeatherData = async (baseUrl, latitude, longitude, apiKey) => {
+  const res = await fetch(`${baseUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`);
+  return await res.json();
+}
+
+/* Function to POST data */
+const postJournalEntry = async(path, data) => {
+  const res = await fetch(path, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      temperature: data.temperature,
+      date: data.date,
+      feelings: data.feelings
+    })
+  });
+}
+
+/* Function to GET Project Data */
+const refreshUI = async() => {
+  const req = await fetch('/all');
+  const allData = await req.json();
+
+  document.getElementById('date').innerHTML = allData.date;
+  document.getElementById('temp').innerHTML = allData.temperature + '°C';
+  document.getElementById('content').innerHTML = allData.feelings;
+}
